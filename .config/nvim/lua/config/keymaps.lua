6e356bd33626b83496fe6f79209a3a1f5061bbc9
mattdevio <magreenberg@fullsail.edu>
2023-03-24 20:54:05 -0600

    feat: window keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -15,3 +15,22 @@ map("v", ">", ">gv")
 -- toggle options
 map("n", "<leader>uf", require("plugins.spec.lsp.io.format").toggle, { desc = "Toggle format on Save" })
 map("n", "<leader>ud", require("util.core").toggle_diagnostics, { desc = "Toggle Diagnostics" })
+
+-- windows
+map("n", "=", function()
+	local w = vim.api.nvim_win_get_width(0)
+	vim.api.nvim_win_set_width(0, w + 5)
+end, { desc = "Vertical +5" })
+map("n", "-", function()
+	local w = vim.api.nvim_win_get_width(0)
+	vim.api.nvim_win_set_width(0, w - 5)
+end, { desc = "Vertical -5" })
+map("n", "<leader>=", function()
+	local h = vim.api.nvim_win_get_height(0)
+	vim.api.nvim_win_set_width(0, h + 5)
+end, { desc = "Horizontal +2" })
+map("n", "<leader>-", function()
+	local h = vim.api.nvim_win_get_height(0)
+	vim.api.nvim_win_set_width(0, h + 5)
+end, { desc = "Horizontal -2" })
+
